7c6daf9576fab733ca802f7ec977fbfc25e217b0
Diego <dsanton@DevOps>
2019-09-20 10:56:09 -0300

    First Commit

---
+++ panel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Busco la cantidad de CPUS
+CPUS=`lscpu | grep "CPU(s):" | grep -v NUMA | tr -d " " | awk '{split($0,a,":");print a[2]}'`
+echo "Number of CPU(s):" $CPUS
+
+# Busco el uso de procesador
+echo "CPU use, last 1 minute  :" `uptime | awk '{split($0,a,"average: "); print a[2]}' | awk '{split($0,b,","); print b[1]","b[2]}'`"/$CPUS"
+echo "CPU use, last 5 minutes :" `uptime | awk '{split($0,a,"average: "); print a[2]}' | awk '{split($0,b,","); print b[3]","b[4]}'`"/$CPUS"
+echo "CPU use, last 15 minutes:" `uptime | awk '{split($0,a,"average: "); print a[2]}' | awk '{split($0,b,","); print b[5]","b[6]}'`"/$CPUS"
+
